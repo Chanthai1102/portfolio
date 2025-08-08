e8d4977f6c3cb0b5ac1a2918a1981081b93a40a4
Locus <chanthaithy.bs2021@gmail.com>
2025-08-09 00:29:55 +0700

    update link for information

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import {FaLinkedin} from "react-icons/fa";
+import {FaGitlab, FaLinkedin} from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import {MdOutlineNightsStay, MdSunny} from "react-icons/md";
@@ -10,13 +10,23 @@ const Navbar = ({ changeTheme, currentTheme }) => {
         <nav className="flex flex-wrap items-center justify-between md:p-2 md:mb-6">
             <div className="flex text-black dark:text-white items-center text-2xl">
                 <button onClick={changeTheme}>
-                    {currentTheme === 'dark' ? <MdSunny /> : <MdOutlineNightsStay />}
+                    {currentTheme === 'dark' ? <MdSunny/> : <MdOutlineNightsStay/>}
                 </button>
             </div>
-            <div className="mx-2 my-6 text-black dark:text-white flex items-center justify-center gap-3 text-2xl md:m-8">
-                <FaLinkedin />
-                <FaGithub />
-                <FaInstagram />
+            <div
+                className="mx-2 my-6 text-black dark:text-white flex items-center justify-center gap-3 text-2xl md:m-8">
+                <a href="https://www.linkedin.com/in/chanthai-thy-72973a282/" aria-label="View my LinkedIn profile">
+                    <FaLinkedin/>
+                </a>
+                <a href="https://github.com/Chanthai1102" aria-label="View my GitHub profile">
+                    <FaGithub/>
+                </a>
+                <a href="https://www.instagram.com/locusiboy/" aria-label="View my Instagram profile">
+                    <FaInstagram/>
+                </a>
+                <a href="https://gitlab.com/Chanthai1102" aria-label="View my GitLab profile">
+                    <FaGitlab/>
+                </a>
             </div>
         </nav>
     );
